d88a30ab6149cbfbb1ad6c9ac76cced7fe2294eb
miranda0111 <75111602+miranda0111@users.noreply.github.com>
2022-02-16 20:42:49 +0800

    Update soy_variable_data.js

--- soy_variable_data.js
+++ soy_variable_data.js
@@ -1,6 +1,9 @@
 module.exports = {"code":200, 
 "variable_data":{ 
-"wkllq":[//这里是悟空浏览器的数据，自己别删了
+"wkllq":[{ //悟空浏览器
+    "device_id":"690909021734094", //链接上找device_id值
+    "x-tt-token":"002e28822057f93fd17c9aed8e7dc2b6fb018c2ff2ae133f5fa39eae767df1bd96f1d24bff79b62138099baefb7d76900e71d5c564c2f7be90ee39c7009d94d9d98ddd1cfcfb2b74cd8349714b11d297af6510101d943c8efb28a588bfe1da94255dc-1.0.1", //请求头的值
+    "sessionid":"2e28822057f93fd17c9aed8e7dc2b6fb"} //请求头cookie上的值
 ],
 "kyin":[//快音
 "10dbfbf9a827575a&eyJleHAiOjE2NDUwMDQ0ODAsImtpZCI6NywiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOiI4OTA3OTY5NCIsIm9wZW5pZCI6Im9ZYlZMeEozNVROTlQxVWl2VXZmd1gzcTRjMWciLCJleHAiOjE2NDUwMDQ0ODB9.9y1nMNVe4K5f8EbODoL1zcnKY35BanBIV9Smz_KxElI&eyJleHAiOjE2NTI3Nzg2ODAsImFsZyI6IkhTMjU2In0.eyJ1aWQiOiI4OTA3OTY5NCIsIm9wZW5pZCI6Im9ZYlZMeEozNVROTlQxVWl2VXZmd1gzcTRjMWciLCJleHAiOjE2NTI3Nzg2ODB9.qhxq3uDhkQ61cOchixAYCoxH0ZqLTDXLLbt-SLahT-Y",//这是第1个账号
