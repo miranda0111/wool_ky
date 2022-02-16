6532df98fc3654f48409123f836b8b9ca711b03c
miranda0111 <75111602+miranda0111@users.noreply.github.com>
2022-02-16 16:16:08 +0800

    提交

---
+++ soy_variable_data.js
@@ -0,0 +1,8 @@
+module.exports = {"code":200, 
+"variable_data":{ 
+"wkllq":[//这里是悟空浏览器的数据，自己别删了
+],
+"kyin":[//快音
+"5b0d05e84311e681&54_qeWzozlRXuSmx4a0aL-70hkq65cGEDcZg5uBcg45OkkR5agY9JzJhr2XmBtsKtv0baNOYsIc-CDpmpOcZAdeJkLYct7BROy28n7chBTtB3g&54_33I8wkQ-qb_0rH3-JgjOw2sJP7KP_x9SfRzaejRSCDZqCJsjv2XOoWMFJo8Xf9dKCZs538Zy1rKj4pMykWOFo-4JIrUrKcfA_svjoTwZSIk",//这是第1个账号
+"",//多账号逗号换行
+]}}
